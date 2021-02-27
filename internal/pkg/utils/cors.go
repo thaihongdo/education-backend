edd5c1e3a7c64c62c3c562695fcf8e02188f38d4
dohongthai <thaihongdo095@gmail.com>
2021-02-27 14:15:09 +0700

    fix cors not working

--- internal/pkg/utils/cors.go
+++ internal/pkg/utils/cors.go
@@ -1,23 +1,21 @@
 package utils
 
 import (
-	"time"
-
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
-
-	return cors.New(cors.Config{
-		AllowOrigins:     []string{},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		/* 	AllowOriginFunc: func(origin string) bool {
-			return origin == "*" // using pattern
-		}, */
-		MaxAge: 12 * time.Hour,
-	})
+	return cors.Default()
+	// return cors.New(cors.Config{
+	// 	AllowOrigins:     config.GetConfig().AllowOrigins,
+	// 	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+	// 	AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	/* 	AllowOriginFunc: func(origin string) bool {
+	// 		return origin == "*" // using pattern
+	// 	}, */
+	// 	MaxAge: 12 * time.Hour,
+	// })
 }
